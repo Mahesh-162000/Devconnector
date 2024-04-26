ac7e6a09f0a58f5a771bd0b5bb9ef0b222c1780f
Mahesh-162000 <maheshchakri42@gmail.com>
2024-04-26 21:35:39 +0530

    Updated Package

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const connectDB = require('./src/config/db');
 const cors = require('cors');
 const path = require('path');
+require('dotenv').config();
 
 const app = express();
 
@@ -23,12 +24,12 @@ app.use('/api/profile', require('./src/routes/api/profile'));
 app.use('/api/posts', require('./src/routes/api/posts'));
 
 // Serve static assets in production
-if (process.env.NOVE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     //Set static Folder
-    app.use(express.static('client-side/build'));
+    app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client-side', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
 }
 
