19b3182cf080e5c5eb19c369243c19dc28eff5d8
Mahesh-162000 <maheshchakri42@gmail.com>
2024-04-27 12:34:41 +0530

    Updated V1

--- server.js
+++ server.js
@@ -33,6 +33,8 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 
+//console.log(path.join(__dirname, 'client', 'build', 'index.html'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
